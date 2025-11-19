ac5fd5b2ff531d50c67ec7bf0f4ca304e8a67828
Daniel Awaritefe <awaritefe@hotmail.co.uk>
2025-11-19 11:49:10 +0000

    favicons

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,4 +8,50 @@ export default defineNuxtConfig({
 		plugins: [tailwindcss()],
 	},
 	modules: ["@nuxt/eslint", "@nuxt/fonts", "@nuxt/image"],
+	app: {
+		head: {
+			link: [
+				// Main favicon
+				{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+
+				// Standard PNG favicons
+				{
+					rel: "icon",
+					type: "image/png",
+					sizes: "16x16",
+					href: "/favicon-16x16.png",
+				},
+				{
+					rel: "icon",
+					type: "image/png",
+					sizes: "32x32",
+					href: "/favicon-32x32.png",
+				},
+
+				// Apple devices
+				{
+					rel: "apple-touch-icon",
+					sizes: "180x180",
+					href: "/apple-touch-icon.png",
+				},
+
+				// Android / PWA icons
+				{
+					rel: "icon",
+					type: "image/png",
+					sizes: "192x192",
+					href: "/android-chrome-192x192.png",
+				},
+				{
+					rel: "icon",
+					type: "image/png",
+					sizes: "512x512",
+					href: "/android-chrome-512x512.png",
+				},
+
+				// Manifest file
+				{ rel: "manifest", href: "/site.webmanifest" },
+			],
+		},
+	},
 });
